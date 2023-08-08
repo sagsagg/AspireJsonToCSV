3f3ff0138d2b67104ed608b14a5dc628da2ac557
Sang Nguyen <sang.n@aspireapp.com>
2023-08-08 23:51:14 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const converter = require('json-2-csv');
 const fs = require('fs');
+const csvParser = require('csv-parser');
 
 const rawJsonData = fs.readFileSync('./data/translation.json');
 const translationJson = JSON.parse(rawJsonData);
@@ -15,6 +16,22 @@ const transformTranslation = () => {
     return outputArray;
 }
 
+const convertCsvToJson = (csvFilePath, callback) => {
+    const jsonArray = [];
+
+    fs.createReadStream(csvFilePath)
+        .pipe(csvParser())
+        .on('data', (row) => {
+            jsonArray.push(row);
+        })
+        .on('end', () => {
+            callback(null, jsonArray);
+        })
+        .on('error', (error) => {
+            callback(error, null);
+        });
+}
+
 const convertJson2CSV = async () => {
     try {
         const csv = await converter.json2csv(transformTranslation());
@@ -24,5 +41,20 @@ const convertJson2CSV = async () => {
         console.log(error);
     }
 };
+
+const convertToSingleObject = (dataArray) => {
+    return dataArray.reduce((resultObject, data) => {
+        resultObject[data["Translation key"]] = data["Bahasa"];
+        return resultObject;
+    }, {});
+}
+
+// convertCsvToJson('./data/translations.csv', (error, jsonData) => {
+//     if (error) {
+//         console.error('Error converting CSV to JSON:', error);
+//     } else {
+//         fs.writeFileSync('translation.json', JSON.stringify(convertToSingleObject(jsonData)))
+//     }
+// });
 
 convertJson2CSV();
